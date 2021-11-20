394e4b7c94cbbaf7d1f84696645ab6068d583416
Cilia Madani <40335630+ciliamadani@users.noreply.github.com>
2021-11-20 13:18:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import cv2 
+
+def load_images():
+
+    return
+
+def load_mask():
+
+    return 
+
+
+def load_intensity():
+
+    return 
+
+def load_vectorS():
+
+    return 
+
+def image_to_greyScale():
+
+
+    return
